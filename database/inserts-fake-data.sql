c1fee5ba7aa34f79c6782d5d1d4579799d6bdc94
Thiago Souza <thiago.souzapassos@gmail.com>
2017-05-10 09:22:57 -0300

    adding some comments about testin and inserting database data

--- database/inserts-fake-data.sql
+++ database/inserts-fake-data.sql
@@ -4,6 +4,7 @@ truncate products;
 truncate orders; 
 truncate orders_has_products;
 
+#all the clients and products are already in our database
 insert into clients values(1, "Thiago", NOW(), NOW());
 insert into clients values(2, "Alex", NOW(), NOW());
 insert into clients values(3, "Zé", NOW(), NOW());
@@ -14,6 +15,7 @@ insert into products values(3, "HD", now(), now());
 insert into products values(4, "Mouse", now(), now());
 insert into products values(5, "keyboard", now(), now());
 
+#new orders will bring info about client
 insert into orders values(1, NOW(), NOW(), 1); #User = 1 Thiago
 insert into orders values(2, NOW(), NOW(), 1);
 insert into orders values(3, NOW(), NOW(), 1);
@@ -23,6 +25,7 @@ insert into orders values(4, NOW(), NOW(), 2); #User = 2 Alex
 insert into orders values(5, NOW(), NOW(), 3); #User = 3 Ze
 insert into orders values(6, NOW(), NOW(), 3);
 
+#and every order will have N products
 #user = 1 Thiago
 insert into orders_has_products values(1,1); #Product = 1 Mobile
 insert into orders_has_products values(2,2); #Product = 2 iPad
